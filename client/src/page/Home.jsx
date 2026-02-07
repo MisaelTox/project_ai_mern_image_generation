8dbbdca44d98d43e22172c652d80e08bb94209a1
MisaelTox <misaeltoxcatl@gmail.com>
2026-02-07 23:34:57 +0100

    fix: hardcode server IP for AWS deployment

--- client/src/page/Home.jsx
+++ client/src/page/Home.jsx
@@ -26,7 +26,7 @@ const Home = () => {
     setLoading(true);
 
     try {
-      const response = await fetch(`${import.meta.env.VITE_SERVER_URL}/api/v1/post`, {
+      const response = await fetch('http://51.20.183.252:8080/api/v1/post', {
         method: 'GET',
         headers: {
           'Content-Type': 'application/json',
